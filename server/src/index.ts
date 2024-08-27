3c7dca567bee4cfee59fc9001e7cc5d77d36b4ec
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-08-27 17:07:43 +0530

    Initialization of prisma and wrote users and post schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,29 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { env } from 'hono/adapter'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+const prisma = new PrismaClient({
+  datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate())
+
+app.post('/api/v1/signup', (c) => {
+  return c.json({ message: "dfas" })
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.json({ message: "dfas" })
+})
+app.post('/api/v1/blog', (c) => {
+  return c.json({ message: "dfas" })
+})
+app.put('/api/v1/blog', (c) => {
+  return c.json({ message: "dfas" })
+})
+
+app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
