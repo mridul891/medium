edf8ce0a51b4f817814a767a5e961a35749bf599
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-08-27 22:30:54 +0530

    done with the sign up api

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,15 +2,46 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { env } from 'hono/adapter'
+import { decode, sign, verify } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    SECRET: string
+  }
+}>()
 
-const prisma = new PrismaClient({
-  datasourceUrl: env.DATABASE_URL,
-}).$extends(withAccelerate())
 
-app.post('/api/v1/signup', (c) => {
-  return c.json({ message: "dfas" })
+app.post('/api/v1/signup', async (c) => {
+
+  // using prisma client to coonnect the connection pool to the database 
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  // taking the body from the request of the user 
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password
+      }
+    })
+
+    // Jwt Implementaion 
+    const secret = c.env.SECRET
+
+    const jwt = await sign({ id: user.id }, secret)
+
+    return c.json({ jwt })
+  } catch (error) {
+    c.status(403)
+    return c.json({ error: "Error while siging up " })
+  }
+
+
+
 })
 
 app.post('/api/v1/signin', (c) => {
