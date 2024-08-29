2e6c5fc784856fa677a660ad9f6d681dbe2b2e9f
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-08-29 21:47:39 +0530

    Sone with sign up route

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,12 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { env } from 'hono/adapter'
-import { decode, sign, verify } from 'hono/jwt'
+import { sign } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string,
-    SECRET: string
+    JWT_SECRET: string
   }
 }>()
 
@@ -25,28 +25,55 @@ app.post('/api/v1/signup', async (c) => {
     const user = await prisma.user.create({
       data: {
         email: body.email,
-        password: body.password
+        password: body.password,
+        name: body.name
+      }
+    })
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET)
+
+    return c.json({ jwt: token })
+
+  } catch (error) {
+    c.status(403)
+    return c.json({ error: "Error while siging ups " })
+  }
+})
+
+
+// signin request
+app.post('/api/v1/signin', async (c) => {
+
+  // Intitaliza the prisma 
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  // taking the body 
+  const body = await c.req.json();
+  try {
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email
+      }, select: {
+        id: true
       }
     })
 
-    // Jwt Implementaion 
-    const secret = c.env.SECRET
+    if (!user) {
+      c.status(403)
+      return c.json({ error: "user not found" })
+    }
 
-    const jwt = await sign({ id: user.id }, secret)
-
-    return c.json({ jwt })
+    const token = sign({ id: user.id }, c.env.SECRET)
   } catch (error) {
     c.status(403)
-    return c.json({ error: "Error while siging up " })
+    return c.json({ error: "Error while siging in " })
   }
-
-
-
 })
 
-app.post('/api/v1/signin', (c) => {
-  return c.json({ message: "dfas" })
-})
+
+
 app.post('/api/v1/blog', (c) => {
   return c.json({ message: "dfas" })
 })
