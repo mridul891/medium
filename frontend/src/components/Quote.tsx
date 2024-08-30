a2d982f61ce963845f5058d7f7e4b1823a44d301
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-08-30 16:50:59 +0530

    started with frontend and completed the backend

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,12 @@
+
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center items-center px-[4rem]">
+        <div className=" text-4xl font-bold text-left">
+            "The Customer support I recieved was exceptional . The support team went above and beyond to address my concerns "
+            <div className="text-left">
+                Julies Winfield
+                <h6>CEO, Acme Inc</h6>
+            </div>
+        </div>
+    </div>
+}
